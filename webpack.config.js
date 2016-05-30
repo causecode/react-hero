5f4d055be889d172201c9d73d395482f175dd274
Nahush Farkande <nahush.farkande@causecode.com>
2016-05-30 17:44:17 +0530

    Css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var isProduction = process.argv.indexOf('--production') != -1;
 
@@ -19,10 +20,21 @@ if (isProduction) {
 	);
 }
 
+plugins.push(
+	new ExtractTextPlugin("style.css", {allChunks: true})
+);
+
 var config = {
-	entry: "./src/index.tsx",
+	entry: [
+		"./src/styles/index.css",
+		"./src/index.tsx",
+		'webpack-dev-server/client?http://localhost:8080',
+		'webpack/hot/only-dev-server'
+	],
 	output: {
-		filename: isProduction ? "./dist/bundle.[hash].min.js" : "./dist/bundle.js"
+		path:"./dist",
+		filename: isProduction ? "bundle.[hash].min.js" : "bundle.js",
+		publicPath: "http://localhost:8080/dist"
 	},
 	devtool: 'source-map',
 	resolve: {
@@ -30,7 +42,9 @@ var config = {
 	},
 	module: {
 		loaders: [
-			{test: /\.tsx?$/, loader: "ts-loader"}
+			{test: /\.tsx?$/, exclude: /(node_modules)/, loaders: ["react-hot", "ts-loader"]},
+			{test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader")},
+			{test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
 		]
 	},
 	plugins: plugins
