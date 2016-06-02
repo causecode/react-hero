0c8f555d1175e65ba5c35c036fbd8148f9035845
Nahush Farkande <nahush.farkande@causecode.com>
2016-06-02 07:29:46 +0530

    Added routing to test implementation.

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,14 @@ import {render} from 'react-dom';
 import {IStore, createStore} from 'redux';
 import {Provider} from "react-redux";
 import {store} from "./store";
+import {NewPage} from "./components/TestImplementations";
+import {Router, Route, browserHistory} from "react-router";
 
 render(
 	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={NewPage} />
+		</Router>
 	</Provider>,
 	document.getElementsByClassName('main-container')[0]
 );
