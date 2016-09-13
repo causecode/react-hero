25e59e2a886ef8084c86d34325d012552d743562
Nahush Farkande <nahush.farkande@causecode.com>
2016-09-13 12:45:25 +0530

    Modified AddReducers to work exactly like combineReducers

--- src/utils/addReducers.ts
+++ src/utils/addReducers.ts
@@ -5,14 +5,14 @@ const formReducer = require<any>('redux-form').reducer;
 import {routerReducer} from 'react-router-redux';
 import {modelReducer as instances} from '../reducers/modelReducer';
 import {dataReducer as data} from '../reducers/data';
+let objectAssign: Function = require<Function>('object-assign');
 
-export const addReducers = (userReducer: Reducer): Reducer => {
-    return combineReducers({
-        userReducer,
+export const addReducers = (ReducerConfig: Object): Reducer => {
+    return combineReducers(objectAssign({}, ReducerConfig, {
         open,
         data,
         instances,
         routing: routerReducer,
         form: formReducer
-    });
+    }));
 };
