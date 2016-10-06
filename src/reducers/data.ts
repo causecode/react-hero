fe550cc17e907ebaa3ca1c869e9a44ba14379a6a
Shishir Anshuman <shishir.anshuman@causecode.com>
2016-10-06 07:59:39 +0530

    fixed if-else in data.ts to store the data in the redux store

--- src/reducers/data.ts
+++ src/reducers/data.ts
@@ -43,10 +43,10 @@ function dataReducer(state = INITIAL_STATE, action ) {
                 let payloadData = action.payload;
                 for (let key in payloadData) {
                     if (payloadData.hasOwnProperty(key)) {
-                        if (payloadData[key].constructor === Array) {
-                            instance[key] = payloadData[key];
-                        } else {
+                        if (payloadData[key].constructor === Object) {
                             (<any>Object).assign(instance, payloadData[key]);
+                        } else {
+                            instance[key] = payloadData[key];
                         }
                     }
                 }
