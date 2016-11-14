c14aace585b0124f8c68e8741d8a16eaf14a9cdb
Nahush Farkande <nahush.farkande@causecode.com>
2016-11-14 16:03:02 +0530

    Code cleanup and restructured the Dynamic scaffolding functionality

---
+++ src/components/Widgets/Input.tsx
@@ -0,0 +1,211 @@
+import * as React from 'react';
+import {
+    FormControl,
+    FormGroup,
+    Col,
+    ControlLabel,
+    Row,
+    Button,
+    ListGroup,
+    ListGroupItem,
+    Radio
+} from 'react-bootstrap';
+import * as moment from 'moment';
+import {isEmpty} from '../../utils/appService';
+import {store} from '../../store/store';
+import {connect} from 'react-redux';
+const {actions} = require<any>('react-redux-form');
+
+export interface IInputProps {
+    model: string;
+    propertyValue: any;
+    type: string;
+    propertyName: string;
+}
+
+let GenericInputTemplate = (props) => {
+    let handleChange = (e: React.FormEvent) => {
+        this.props.onChange(e.target[`value`]);
+    };
+
+    return (
+        <input className="form-control" {...props} value={props.propertyValue} onChange={handleChange}/>
+    );
+};
+
+let BooleanInputTemplate = (props) => {
+    
+    let handleChange = (e: React.FormEvent) => {
+        this.props.onChange((e.target[`value`] === 'option-true'));
+    };
+
+    return (
+        <Row>
+            <Col sm={6}>
+                <Radio
+                    onChange={handleChange}
+                    value="option-true"
+                    name={props.propertyName}
+                    checked={props.propertyValue}>
+                    True
+                </Radio>
+            </Col>
+            <Col sm={6}>
+                <Radio
+                    onChange={handleChange}
+                    value="option-false"
+                    name={props.propertyName}
+                    checked={!props.propertyValue}>
+                    False
+                </Radio>
+            </Col>
+        </Row>
+    );
+};
+
+let DropDownInputTemplate = (props) => {
+
+    let handleChange = (e: React.FormEvent) => {
+        props.onChange(e.target[`value`]);
+    };
+    
+    return (
+        <select
+            value={props.propertyValue}
+            className="form-control"
+            onChange={handleChange}>
+            <option
+                disabled
+                value=""
+                style={{
+                color: 'grey',
+                pointerEvents: 'none'
+            }}>Select one..</option>
+            {(() => {
+                let enumInstance = props.enum;
+                if (isEmpty(enumInstance)) {
+                    return;
+                }
+                return Object
+                    .keys(enumInstance)
+                    .map((enumproperty : string, index : number) => {
+                        if (enumInstance.hasOwnProperty(enumproperty) && !isNaN(parseInt(enumproperty, 10))) {
+                            return (
+                                <option key={`enumproperty-key-${index}`} value={enumproperty}>
+                                    {enumInstance[enumproperty]}
+                                </option>
+                            );
+                        }
+                    });
+            })()}
+        </select>
+    );
+};
+
+class ListInputTemplate extends React.Component<any, any> {
+
+    constructor(props) {
+         super(props);
+         this.state = {text: ''};
+    }
+
+    handleTextChange = (e: React.FormEvent) => {
+        this.setState({newListItem: e.target[`value`]});
+    }
+
+    addListItem = (e: React.FormEvent) => {
+        let propertyValue = this.props.propertyValue ? this.props.propertyValue.slice() : [] ;
+        propertyValue.push(this.state.newListItem);
+        this.props.onChange(propertyValue);
+    }
+    
+    render() {
+        let list = this.props.propertyValue || ['Nothing to show.'];
+        return (
+            <div>
+                <Row>
+                    <Col sm={8}>
+                        <FormControl
+                            type="text"
+                            value={this.state.newListItem}
+                            onChange={this.handleTextChange}/>
+                    </Col>
+                    <Col sm={4}>
+                        <Button bsStyle="default" onClick={this.addListItem}>Add</Button>
+                    </Col>
+                </Row> 
+                <ListGroup style={{margin: '10px'}}> 
+                {(() => {
+                    return list.map((listItem : string, index : number) => {
+                        return (
+                            <ListGroupItem
+                                style={{
+                                wordWrap: 'break-word'
+                            }}
+                                key={`${this.props.propertyName}-${index}`}>
+                                {listItem}
+                            </ListGroupItem>
+                        );
+                    });
+                })()} 
+                </ListGroup>
+            </div>
+        );        
+    }
+}
+
+class FormInputImpl extends React.Component<IInputProps, {}> {
+    
+    handleChange = (newValue: any) => {
+        store.dispatch(actions.change(this.props.model, newValue));
+    }
+
+    getInputTemplate = (): React.ComponentClass<any> | React.StatelessComponent<any> => {
+        let type: string = this.props.type;
+        switch (type) {
+            case 'boolean': return BooleanInputTemplate;
+            case 'select': return DropDownInputTemplate;
+            case 'list': return ListInputTemplate;
+            default: return GenericInputTemplate;
+        }
+    }
+
+    render() {
+        let {propertyValue} = this.props;
+        if (this.props.type === 'date') {
+            propertyValue = moment(propertyValue).format('YYYY-MM-DD');
+        }
+        let InputTemplate = this.getInputTemplate();
+        return (
+            <FormGroup className="row" style={{margin: '0px'}}>
+                <Col sm={3}>
+                    <ControlLabel style={{textAlign: 'right'}}>{this.props.propertyName}</ControlLabel>
+                </Col>
+                <Col sm={4}>
+                    <InputTemplate 
+                        {...this.props}
+                        onChange={this.handleChange}
+                    />
+                </Col>
+            </FormGroup>
+        );
+    }
+}
+
+let mapStateToProps = (state, ownProps) => {
+    let value; 
+    let data = state.forms;
+    ownProps.model.split('.').forEach(prop => {
+        let keys = ownProps.model.split('.');
+        if (keys.indexOf(prop) === (keys.length - 1)) {
+            value = data[prop];
+        }
+        data = data[prop];
+    });
+    return {
+        propertyValue: data
+    };
+};
+
+let FormInput = connect(mapStateToProps)(FormInputImpl);
+export {FormInput};
--- src/utils/appService.tsx
+++ src/utils/appService.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
 import {BaseModel, IPropTypes} from '../models/BaseModel';
+import {ModelPropTypes} from '../models/ModelPropTypes';
 import {fromJS} from 'immutable';
 import {store} from '../store/store';
-import { ControlLabel, Col, FormGroup } from 'react-bootstrap';
-import {
-    ListInput,
-    BooleanInput,
-    NumberInput,
-    StringInput,
-    DateInput,
-    DropDownInput
-} from '../components/Widgets';
+import {ControlLabel, Col, FormGroup} from 'react-bootstrap';
+import {FormInput} from '../components/Widgets';
 const {actions} = require<any>('react-redux-form');
 
 declare module process {
@@ -36,7 +30,7 @@ export function isEmpty(obj: Object): boolean {
     return (!obj || !Object.keys(obj).length);
 }
 
-export function getModelString(...args: any[]): string{
+export function getModelString(...args: any[]): string {
     return `RHForms.${args.join('.')}`;
 }
 
@@ -67,40 +61,19 @@ export function generateSubForm(propertyName: string, object: Object, propTypes:
 
     let FormControls =  Object.keys(object).map((prop, index: number) => {
         if (object.hasOwnProperty(prop)) {
-            let FormControl;
-            // let propertyValue = object[prop];
-            switch (propTypes[prop].type) {
-                case 'ARRAY':
-                    FormControl = ListInput;
-                    break;
-                case 'BOOLEAN':
-                    FormControl = BooleanInput;
-                    break;
-                case 'NUMBER':
-                    FormControl = NumberInput;
-                    break;
-                case 'DATE':
-                    FormControl = DateInput;
-                    break;
-                case 'ENUM':
-                    FormControl = DropDownInput;
-                    break;
-                default:
-                    FormControl = StringInput;
-                    break;
+            let type: string = propTypes[prop].type;
+            if (type === 'object') {
+                type = ModelPropTypes.stringInputType;
             }
-
-            if (FormControl) {
-                return (
-                    <FormControl
-                        enum={propTypes[prop].enum}
-                        key={`form-control-sub-${propertyName}-${index}`}
-                        // propertyValue={propertyValue}
-                        propertyName={prop} 
-                        model={model + '.' + prop}
-                    />
-                );
-            }
+            return (
+                <FormInput
+                    type={propTypes[prop].type}
+                    enum={propTypes[prop].enum}
+                    key={`form-control-sub-${propertyName}-${index}`}
+                    propertyName={prop} 
+                    model={model + '.' + prop}
+                />
+            );
         }
     });
 
@@ -120,64 +93,33 @@ export function generateSubForm(propertyName: string, object: Object, propTypes:
 
 export function generateForm<T extends BaseModel>(
         instance: T,
-        changeHandler: (instance: BaseModel) => void,
         model: string = '',
         propTypes: IPropTypes = instance.propTypes
 ): JSX.Element {
-
     return (
         <div>
             {Object.keys(propTypes).map((prop: string, index: number) => {
-                
-                let FormControl: React.ComponentClass<any>;
                 let keyPath = model ? model + '.' + prop : prop; 
-
-                // Getting propertyValue this way to account for nested objects.
                 let propertyValue = getIn(instance.properties, keyPath);
-
-                switch (instance.propTypes[prop].type) {
-                    case 'ARRAY': 
-                        FormControl = ListInput;
-                        break;
-                    case 'BOOLEAN':
-                        FormControl = BooleanInput; 
-                        break;
-                    case 'NUMBER': 
-                        FormControl = NumberInput; 
-                        break;
-                    case 'OBJECT': 
-                        return generateSubForm(
-                                prop, 
-                                propertyValue, 
-                                getIn(instance.propTypes, keyPath).propTypes, 
-                                getModelString(instance.resourceName, keyPath)
-                        );
-                    case 'DATE':
-                        FormControl = DateInput;
-                        break;
-                    case 'ENUM': 
-                        FormControl = DropDownInput;
-                        break;
-                    default: 
-                        FormControl = StringInput;
-                        break;
-                }
-
-                
-                if (FormControl) {
-                    return (
-                        <FormControl
-                            enum={instance.propTypes[prop].enum}
-                            onChange={changeHandler}
-                            // instance={instance}
-                            key={`form-control-${instance.resourceName}-${index}`}
-                            // propertyValue={propertyValue}
-                            propertyName={prop} 
-                            model={getModelString(instance.resourceName, keyPath)}
-                        />
+                let type: string = instance.propTypes[prop].type;
+                if (type === ModelPropTypes.objectInputType) {
+                    return generateSubForm(
+                            prop, 
+                            propertyValue, 
+                            getIn(instance.propTypes, keyPath).propTypes, 
+                            getModelString(instance.resourceName, keyPath)
                     );
                 }
-                
+            
+                return (
+                    <FormInput
+                        type={instance.propTypes[prop].type}
+                        enum={instance.propTypes[prop].enum}
+                        key={`form-control-${instance.resourceName}-${index}`}
+                        propertyName={prop} 
+                        model={getModelString(instance.resourceName, keyPath)}
+                    />
+                );
             })}
         </div>
     );
