b7356c9838604d00539b58c3dc5955184cbe209b
Nahush Farkande <nahush.farkande@causecode.com>
2016-11-28 19:12:56 +0530

    Updated addReducers

--- src/utils/addReducers.ts
+++ src/utils/addReducers.ts
@@ -1,16 +1,19 @@
 import {open} from '../reducers/open';
 import {combineReducers} from 'redux';
 import {Reducer} from 'redux';
-const formReducer = require<any>('redux-form').reducer;
 import {routerReducer} from 'react-router-redux';
 import {dataReducer as data} from '../reducers/data';
+// const formReducer = require<any>('redux-form').reducer;
 let objectAssign: Function = require<Function>('object-assign');
+const {combineForms} = require<any>('react-redux-form');
+const reduxFormReducer = require<any>('redux-form').reducer;
 
 export const addReducers = (ReducerConfig: Object): Reducer => {
     return combineReducers(objectAssign({}, ReducerConfig, {
         open,
         data,
         routing: routerReducer,
-        form: formReducer
+        form: reduxFormReducer,
+        forms: combineForms({RHForms: {}})
     }));
 };
