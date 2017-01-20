28785b1e3fef6ec77e50841f288ca506dd5e4005
Hardik Modha <hardik.modha@causecode.com>
2017-01-20 18:27:10 +0530

    Addec custom Datetime component.

--- src/components/Widgets/Input.tsx
+++ src/components/Widgets/Input.tsx
@@ -14,6 +14,7 @@ import {isEmpty, parseWidgetDate} from '../../utils/appService';
 import {store} from '../../store';
 import {connect} from 'react-redux';
 const {actions} = require<any>('react-redux-form');
+const ReactDatetime = require<any>('react-datetime');
 
 export interface IInputProps {
     model: string;
@@ -28,7 +29,7 @@ let GenericInputTemplate = (props) => {
     };
 
     return (
-        <input type={props.type} className="form-control" value={props.value} onChange={handleChange}/>
+        <input type={props.type} className="form-control" value={props.propertyValue} onChange={handleChange}/>
     );
 };
 
@@ -74,29 +75,27 @@ let DropDownInputTemplate = (props) => {
             className="form-control"
             onChange={handleChange}>
             <option
-                disabled
                 value=""
                 style={{
                 color: 'grey',
                 pointerEvents: 'none'
-            }}>Select one..</option>
+            }}>Select One</option>
             {(() => {
                 let enumInstance = props.enum;
                 if (isEmpty(enumInstance)) {
                     return;
                 }
-                return Object
-                    .keys(enumInstance)
-                    .map((enumproperty : string, index : number) => {
-                        if (enumInstance.hasOwnProperty(enumproperty) && !isNaN(parseInt(enumproperty, 10))) {
-                            
-                            return (
-                                <option key={`${enumInstance[enumproperty]}-${index}`} value={enumproperty}>
-                                    {enumInstance[enumproperty]}
-                                </option>
-                            );
-                        }
-                    });
+                let optionElements: JSX.Element[] = [];
+                enumInstance.forEach((element, index) => {
+                    optionElements.push(
+                        <option
+                            key={index}
+                            value={element.value}>
+                            {element.label}
+                        </option>
+                    );
+                });
+                return optionElements;
             })()}
         </select>
     );
@@ -154,6 +153,28 @@ class ListInputTemplate extends React.Component<any, any> {
     }
 }
 
+// TODO: Make it generic component by allowing it to accept all props of react-datetime
+// TODO: Add value field to pre populdate it with a value.
+class DateTimeComponent extends React.Component<IInputProps, void> {
+    
+    handleChange = (newValue: any) => {
+        if (newValue && newValue._d) {
+            store.dispatch(actions.change(this.props.model, newValue._d.toISOString()));
+        }
+    }
+
+    render() {
+        return(
+           <ReactDatetime 
+                strictParsing={true}
+                utc={true}
+                onChange={this.handleChange}
+           />
+        );
+    }
+
+}
+
 class FormInputImpl extends React.Component<IInputProps, {}> {
     
     handleChange = (newValue: any) => {
@@ -166,6 +187,7 @@ class FormInputImpl extends React.Component<IInputProps, {}> {
             case 'boolean': return BooleanInputTemplate;
             case 'select': return DropDownInputTemplate;
             case 'list': return ListInputTemplate;
+            case 'datetime': return DateTimeComponent;
             default: return GenericInputTemplate;
         }
     }
