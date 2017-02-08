8137246a6b2cb2139f03860195db6cfb11a336e7
Shishir Anshuman <shishir.anshuman@causecode.com>
2017-02-08 11:41:56 +0530

    Removed source, modules and typings files.

--- src/cli/getCreatePage.ts
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env node
-
-import {getEditPage} from './generatorService';
-
-getEditPage('create');
--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './modelService';
-export * from './componentService';
-export * from './appService';
-export * from './addReducers';
-import * as StoreService from './storeService';
-
-export {StoreService};
-
