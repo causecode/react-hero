90a2dd1ec3751b542a537dfcffe2ec32c71cf044
Hardik Modha <hardik.modha@causecode.com>
2017-02-28 17:18:57 +0530

    Added test cases for ReactSelect and few TSLint fixes.

--- src/components/common/ReactSelect.tsx
+++ src/components/common/ReactSelect.tsx
@@ -12,34 +12,29 @@ export interface IReactSelectProps {
     input?: {onChange: (value: string) => void, onBlur: (value: string) => void, value: any};
 }
 
-export interface ISelectProps {
+export interface ISelectProps extends IReactSelectProps {
     name: string;
-    multi: boolean;
-    options: IDropDownFilterData[];
     value: string;
-    onInputChange?: (value: string) => void;
-    style?: CSS;
-    input?: {onChange: (value: string) => void, onBlur: (value: string) => void, value: any};
 }
 
 export class ReactSelect extends React.Component<IReactSelectProps, void> {
-    
+
     render(): JSX.Element {
-        
+
         let selectProps: ISelectProps = {
             name: 'select',
             multi: this.props.multi,
             options: this.props.options,
             value: this.props.input.value || '',
-            style: this.props.style
+            style: this.props.style,
         };
-       
+
         if (this.props.onInputChange) {
             selectProps.onInputChange = this.props.onInputChange;
         }
 
         return (
-            <Select 
+            <Select
                     {...this.props}
                     value={this.props.input.value || ''}
                     onChange={(value) => {
@@ -49,7 +44,7 @@ export class ReactSelect extends React.Component<IReactSelectProps, void> {
                     onBlur={() => {
                         this.props.input.onBlur(this.props.input.value);
                     }}
-                    options={this.props.options}        
+                    options={this.props.options}
             />
         );
     }
