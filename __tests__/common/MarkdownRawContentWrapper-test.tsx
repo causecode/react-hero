8b08451641dbae4765176b7101d4f8fdfeac068a
Sachidanand Vaishnav <sachidanandv@causecode.com>
2017-04-13 10:33:57 +0530

    Fixed indentation issues. #14

--- __tests__/common/MarkdownRawContentWrapper-test.tsx
+++ __tests__/common/MarkdownRawContentWrapper-test.tsx
@@ -2,17 +2,17 @@ jest.mock('react-markdown-editor');
 jest.unmock('../../src/components/common/MarkdownWrapper');
 
 import * as React from 'react';
-import {Provider} from 'react-redux';
-import {ShallowWrapper, shallow, ReactWrapper, mount} from 'enzyme';
-import {configureStore, store} from '../../src/store';
+import { Provider } from 'react-redux';
+import { ShallowWrapper, shallow, ReactWrapper, mount } from 'enzyme';
+import { configureStore, store } from '../../src/store';
 import {
-    MarkdownWrapper, 
-    IMarkdownProps, 
+    MarkdownWrapper,
+    IMarkdownProps,
     MarkdownWrapperImpl,
 } from '../../src/components/common/MarkdownWrapper';
 import {
-    RawContentWrapper, 
-    IRawContentProps, 
+    RawContentWrapper,
+    IRawContentProps,
     RawContentWrapperImpl,
 } from '../../src/components/common/RawContentWrapper';
 
@@ -22,7 +22,7 @@ let unroll: any = require('unroll');
 unroll.use(it);
 
 describe('Markdown and RawContent Wrapper test cases', (): void => {
-   
+
     let saveData: jest.Mock<void> = jest.fn<void>();
     let text: string = `Test Data`;
 
@@ -35,50 +35,58 @@ describe('Markdown and RawContent Wrapper test cases', (): void => {
     );
 
     let rawContent: ShallowWrapper<IRawContentProps, void> = shallow<IRawContentProps, void>(
-        <RawContentWrapperImpl
-            model={text}
-            value={text}
-            saveData={saveData}
-        />
+            <RawContentWrapperImpl
+                    model={text}
+                    value={text}
+                    saveData={saveData}
+            />
     );
 
     unroll('Component should contain #selector element', (
-        done: () => void, 
-        args: {key: ShallowWrapper<IRawContentProps | IMarkdownProps, void>, 
-                    selector: React.ComponentClass<any> | string}): 
-        void => {
+        done: () => void,
+        args: {
+            key: ShallowWrapper<IRawContentProps | IMarkdownProps, void>,
+            selector: React.ComponentClass<any> | string
+        }): void => {
         expect(args.key.find(args.selector).length).toEqual(1);
-            done();
+        done();
     }, [
         ['key', 'selector'],
         [rawContent, 'textarea'],
         [markDown, MarkdownEditor],
     ]);
-    
+
 
     it('When changes made in testarea should call handleChange', () => {
-        rawContent.find('textarea').simulate('change', {target: {value: text}});
+        rawContent.find('textarea').simulate('change', { target: { value: text } });
         expect(saveData).toBeCalledWith(text, text);
     });
 
     describe('When the component is connected to redux store', (): void => {
         let markdown: ReactWrapper<IMarkdownProps, void> = mount<IMarkdownProps, void>(
-            <Provider store={configureStore({
-                forms: {test: {data: text}},
-            })}>
-                <MarkdownWrapper model={text} />
-            </Provider>
+                <Provider store={configureStore({
+                    forms: { test: { data: text } },
+                })}>
+                    <MarkdownWrapper model={text} />
+                </Provider>
         );
 
         let rawContent: ReactWrapper<IRawContentProps, void> = mount<IRawContentProps, void>(
-            <Provider store={configureStore({
-                forms: {test: {data: text}},
-            })}>
-                <RawContentWrapper model={text}/>
-            </Provider>
+                <Provider store={configureStore({
+                    forms: { test: { data: text } },
+                })}>
+                    <RawContentWrapper model={text} />
+                </Provider>
         );
 
-        unroll('it should render #componentName', (done: () => void, args): void => {
+        unroll('it should render #componentName', (
+            done: () => void,
+            args: {
+                componentName: string,
+                component: ReactWrapper<IRawContentProps, void>,
+                innerComponent: React.ComponentClass<any>
+            }):
+            void => {
             expect(args.component.find(args.innerComponent).length).toBe(1);
             done();
         }, [
