0eb63773ab0031f587013d896682802f903a4321
Saurav Biswas <saurav.biswas@causecode.com>
2017-09-06 13:16:42 +0530

    Fixed directory paths

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import * as appService from './utils/appService';
-let packageJson: any = require('../../../package.json');
-let localConfigJson: any = require('../../../localConfig.json');
+let packageJson: any = require('../package.json');
+let localConfigJson: any = require('../localConfig.json');
 
 // Doing this to avoid cyclic imports problem when used through commandline.
 let isEmpty: (...args: any[]) => void | boolean = appService.isEmpty || ((...args: any[]) => {});
