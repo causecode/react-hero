8f33f30c08ab1f40b3aab0769df43dd5541b8904
Shivam Pandey <shivam.pandey@causecode.com>
2018-12-20 15:50:16 +0530

    published release for the 2.0.6

--- __mocks__/fileMock.js
+++
@@ -1,6 +0,0 @@
-/*
- * This file is used to mock files with extensions .ttf, .eot, .svg, .gif while writing tests.
- * Please look at the moduleNameMapper field in package.json
- */
-
-module.exports = '';
